274fe5fb491c9560f58c258c5cc1d4a1a08173bd
m-vishnuraj <mvishnurajm@gmail.com>
2023-01-05 19:50:38 +0530

    screens media query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1 className='text-2xl text-center pt-3 text-ftc_color'>Welcome to Leo Electrodynamics</h1>
+            <h1 className='text-2xl text-center pt-3 text-ftc_color xl:text-red-600'>Welcome to Leo Electrodynamics</h1>
         </div>
     )
 }
