f69b5659f6733ca272c446371c97c077172480d7
m-vishnuraj <mvishnurajm@gmail.com>
2023-01-05 19:34:06 +0530

    basic completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-center pt-3 text-ftc_color'>Welcome to Leo Electrodynamics</h1>
+        </div>
+    )
+}
+
+export default Home
